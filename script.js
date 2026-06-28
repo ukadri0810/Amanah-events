e1e842cb3b9d19534f6155470f8454cf0c258584
ukadri0810 <ukadri0810@gmail.com>
2026-06-28 10:46:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-menuButton?.addEventListener('click', () => navLinks.classList.toggle('open'));
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => navLinks.classList.remove('open'));
-});
